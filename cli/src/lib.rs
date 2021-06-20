68ec4bc903fb0a10f5cf1106eefdfa7b6522edf5
Inver <jankowskipawelxd@gmail.com>
2021-06-20 12:05:25 +0200

    Rename main command

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3,12 +3,12 @@ use clap::{Clap, AppSettings};
 #[derive(Clap)]
 #[clap(version = "0.0.1", author = "Paweł (Inver) Jankowski")]
 #[clap(setting = AppSettings::ColoredHelp)]
-struct Commands {
+struct Main {
     input: String,
 }
 
 pub fn execute() {
-    let command = Commands::parse();
+    let command = Main::parse();
 
     println!("Hello {}", command.input);
 }
