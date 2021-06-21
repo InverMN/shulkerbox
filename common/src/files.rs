28e68d4ba1dfd8f543f336d3e5a4c77acd1e4a10
Inver <jankowskipawelxd@gmail.com>
2021-06-21 11:37:14 +0200

    Add const APP_DIRECTORY

--- common/src/files.rs
+++ common/src/files.rs
@@ -3,31 +3,31 @@ use std::fs::*;
 use std::io::Result;
 use crate::copy_file;
 
+pub const APP_DIRECTORY: &str = &format!("{}/.shulkerbox", var("HOME").unwrap());
+
 pub fn mantain_file_structure() -> Result<()> {
     create_dirs("servers")?;
     create_dirs("installers")?;
     create_dirs("defaults")?;
-    copy_file!("../../resources/eula.txt", ".shulkerbox/defaults/eula.txt");
-    copy_file!("../../resources/server.properties", ".shulkerbox/defaults/server.properties");
+    copy_file!("../../resources/eula.txt", "defaults/eula.txt");
+    copy_file!("../../resources/server.properties", "defaults/server.properties");
 
     Ok(())
 }
 
 pub fn create_dirs(relative_path: &str) -> Result<()> {
-    let home_path = var("HOME").unwrap(); 
-    create_dir_all(format!("{}/.shulkerbox/{}", home_path, relative_path))?;
+    create_dir_all(format!("{}/{}", APP_DIRECTORY, relative_path))?;
     Ok(())
 }
 
 #[macro_export]
 macro_rules! copy_file {
     ($source_path: expr, $target_path: expr) => {
-        let home_path = var("HOME").unwrap(); 
         let _result: ::std::io::Result<()> = match ::std::fs::metadata($target_path) {
             Ok(_) => Ok(()),
             Err(_) => {
                 let content = include_str!($source_path);
-                ::std::fs::write(format!("{}/{}", home_path, $target_path), content)?;
+                ::std::fs::write(format!("{}/{}", APP_DIRECTORY, $target_path), content)?;
                 Ok(())
             },
         };
